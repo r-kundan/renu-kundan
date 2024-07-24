977a840279a17dc7de280c9645d3553303c9bcd8
apple <apple@apples-MacBook-Pro-2.local>
2024-07-24 21:10:41 +0530

    bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import EducationPAge from "@/app/education/page"
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full bg-black/[0.96] flex flex-col items-center gap-20 antialiased bg-grid-white/[0.02]">
+    <main className="min-h-screen w-full bg-black/[0.96] flex flex-col items-center gap-20 antialiased ">
 
 <HeroSection/>
 <EducationPAge/>
